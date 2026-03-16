6bc623d8b35c10dcb92ab16cadc2c1b911f8dbcc
Hamza Husic <hamzahusic.dev@gmail.com>
2026-03-16 15:58:43 +0100

    Created MVVM presentation layer

--- app/src/main/java/com/example/medicationreminderapp/presentation/theme/Color.kt
+++ app/src/main/java/com/example/medicationreminderapp/presentation/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.medicationreminderapp.ui.theme
+package com.example.medicationreminderapp.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/medicationreminderapp/presentation/theme/Type.kt
+++ app/src/main/java/com/example/medicationreminderapp/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.medicationreminderapp.ui.theme
+package com.example.medicationreminderapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
